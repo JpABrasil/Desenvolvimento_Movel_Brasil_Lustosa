551bfd10b7e3eb706640055c2afbf7cddcafd7db
JpABrasil <joaopedroalexandrino6@hotmail.com>
2025-02-06 23:11:20 -0300

    Inicio

---
+++ app/src/main/java/com/example/projetofaculdade/TelaInicial.kt
@@ -0,0 +1,10 @@
+package com.example.projetofaculdade
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TelaInicial : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tela_inicial) // Define o layout da tela principal
+    }
+}
